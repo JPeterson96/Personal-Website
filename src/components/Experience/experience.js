8934c13a849446ee2ba659779aab558cf7f5a4be
Jamie <jamie215215@hotmail.com>
2023-09-19 11:36:11 -0500

    more styling changes and removing unused code

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -1,29 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './experience.css';
-import ZoomInIcon from '@mui/icons-material/ZoomIn';
-import ZoomOutIcon from '@mui/icons-material/ZoomOut';
 
 function Experience() {
-  const [zoom, setZoom] = useState(100);
-
-  const handleZoomIn = () => {
-    if (zoom < 200) {
-      setZoom(zoom + 10);
-    }
-  };
-
-  const handleZoomOut = () => {
-    if (zoom > 50) {
-      setZoom(zoom - 10);
-    }
-  };
+  const zoom = 100;
 
   return (
     <div className="experienceCcontainer">
-      <div className="experienceHeader">
-        <ZoomOutIcon className='iconStyling' fontSize='large' variant="contained" style={{marginRight:"1%"}} onClick={() => handleZoomOut()}/>
-        <ZoomInIcon className='iconStyling' fontSize='large' variant="contained" style={{marginLeft:"1%"}} onClick={() => handleZoomIn()}/>
-      </div>
       <div className="pdfDocument">
         <iframe
           title="PDF Resume"
